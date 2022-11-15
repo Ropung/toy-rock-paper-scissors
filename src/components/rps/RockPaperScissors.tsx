a0e36d302da8625f3514025f7cce3bdc80058792
Gihun <563710@naver.com>
2022-11-16 03:25:19 +0900

    (퇴근용 커밋)

--- src/components/rps/RockPaperScissors.tsx
+++ src/components/rps/RockPaperScissors.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import ResponsibleCircle from "@styles/svg/ResponsibleCircle";
 
 const RockPaperScissors = () => {
-  return <div>RockPaperScissors</div>;
+  return (
+    <div>
+      <div className="flex gap-4">
+        <ResponsibleCircle className="w-[10vw]" />
+        <ResponsibleCircle className="w-[10vw]" />
+        <ResponsibleCircle className="w-[10vw]" />
+        <ResponsibleCircle className="w-[10vw]" />
+      </div>
+    </div>
+  );
 };
 
 export default RockPaperScissors;

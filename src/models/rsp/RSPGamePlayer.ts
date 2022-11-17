4f4b58e5a1b6e9a0bcc1b0c8b350c36471c69e36
Gihun <563710@naver.com>
2022-11-18 03:05:06 +0900

    add(player): Adding an interface to add a key to a map

--- src/models/rsp/RSPGamePlayer.ts
+++ src/models/rsp/RSPGamePlayer.ts
@@ -2,6 +2,7 @@ import MatchResult from "./MatchResult";
 import RockScissorsPaper from "./RockScissorsPaper";
 
 export default interface RSPGamePlayer {
+  userid: number | null;
   name: string;
   // 프로젝트에 내장된 아바타를 사용하는 경우 avatar라고 하는 custom scheme을 사용(avatar:)
   avatarUri: string;

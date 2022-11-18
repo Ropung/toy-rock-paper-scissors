03393aa9015baf3613e6c9e83715be91f9160dcb
Gihun <563710@naver.com>
2022-11-19 04:21:09 +0900

    feat(keyborad): creat keyborad

--- src/components/rsp/map/RSPPianoMap.tsx
+++ src/components/rsp/map/RSPPianoMap.tsx
@@ -1,33 +1,81 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const RSPPianoMap = () => {
-  const whiteKeyboard = "w-16 h-full bg-white";
-  const blackKeyboard = "w-16 h-full bg-black";
+  const whiteKeyboard = "w-[10vw] max-w-[5rem] h-full bg-white";
+  const blackKeyboard = "w-[10vw] max-w-[5rem] h-full bg-black";
+
+  const keyboradRef = useRef<HTMLDivElement | null>(null);
+  const whiteKeyULRef = useRef<HTMLUListElement | null>(null);
+  const blackKeyULRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    const setKeyboardDivSize = () => {
+      if (
+        !keyboradRef.current ||
+        !whiteKeyULRef.current ||
+        !blackKeyULRef.current
+      ) {
+        //
+        const error = new Error("연결되지 않은 Ref가 있습니다.");
+        console.error(error.stack);
+        return;
+      }
+
+      keyboradRef.current.style.width =
+        whiteKeyULRef.current.offsetWidth + "px";
+
+      blackKeyULRef.current.style.transform = `translateX(${
+        // 다음 코드의 의존도: 흰 건반이 딱 8개고, 검은 건반의 시작점은 흰 건반 하나의 절반 사이즈. 즉 흰 건반 UL의 1/16 사이즈
+        // (whiteKeyULRef.current.offsetWidth >> 4) + 2
+        // 고친다면:
+        ((whiteKeyULRef.current.firstChild as HTMLElement).offsetWidth >> 1) + 2
+      }px)`;
+
+      console.log(
+        "test",
+        keyboradRef.current.style.width,
+        blackKeyULRef.current.style.transform
+      );
+    };
+
+    setKeyboardDivSize();
+
+    window.onresize = (evt) => {
+      setKeyboardDivSize();
+    };
+  }, []);
+  // onresize = (event) => {};
 
   return (
     <div className="w-full h-full">
       <div className="w-full h-[50%] bg-black bg-opacity-20 flex items-center justify-center">
-        {/* 흑건 Container*/}
-        <div className="relative w-full h-[80%]">
-          {/* 흑건 List  */}
-          <ul className="absolute z-10 grid grid-cols-7 w-fit h-[60%] items-center gap-1 justify-start pl-8">
-            <li className={`${blackKeyboard}`}> </li>
-            <li className={`${blackKeyboard}`}> </li>
-            <li className={`${blackKeyboard} col-start-4`}> </li>
-            <li className={`${blackKeyboard}`}> </li>
-            <li className={`${blackKeyboard}`}> </li>
-            <li className={`${blackKeyboard}`}> </li>
+        {/* 건반 Container*/}
+        <div ref={keyboradRef} className="relative h-[80%]">
+          {/* 흑건 6 List  */}
+          <ul
+            ref={blackKeyULRef}
+            className="absolute z-10 grid grid-cols-7 w-fit h-[60%] items-center gap-1 justify-start left-0"
+          >
+            <li className={`${blackKeyboard}`} />
+            <li className={`${blackKeyboard}`} />
+            <li className={`${blackKeyboard} col-start-4`} />
+            <li className={`${blackKeyboard}`} />
+            <li className={`${blackKeyboard}`} />
+            <li className={`${blackKeyboard}`} />
           </ul>
-          <ul className="absolute z-0 flex w-full h-full flex-row gap-1 items-center justify-start">
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}> </li>
-            <li className={`${whiteKeyboard}`}></li>
+          {/* 백건 8 List */}
+          <ul
+            ref={whiteKeyULRef}
+            className="absolute z-0 flex w-fit h-full flex-row gap-1 items-center justify-start"
+          >
+            <li className={`${whiteKeyboard}`} />
+            <li className={`${whiteKeyboard}`} />
+            <li className={`${whiteKeyboard}`} />
+            <li className={`${whiteKeyboard}`} />
+            <li className={`${whiteKeyboard}`} />
+            <li className={`${whiteKeyboard}`} />
+            <li className={`${whiteKeyboard}`} />
+            <li className={`${whiteKeyboard}`} />
           </ul>
         </div>
       </div>

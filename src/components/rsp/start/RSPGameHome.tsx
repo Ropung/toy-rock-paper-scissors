dbd0c5499aed403795f934c74ed40946bc47eb56
Gihun <563710@naver.com>
2022-11-20 02:54:49 +0900

    style(rsp): adjust style home

--- src/components/rsp/start/RSPGameHome.tsx
+++ src/components/rsp/start/RSPGameHome.tsx
@@ -6,7 +6,7 @@ const RSPGameHome = () => {
   const [isStart, setStart] = useState<boolean>(false);
 
   return (
-    <div className="w-screen h-screen bg-main-contra">
+    <div className="w-full h-[85vh] bg-main-contra">
       {!isStart ? (
         <RSPGameStartPage setStart={setStart} />
       ) : (

531841d5be9cae96f935ecf6adf4f657a8edee61
Gihun <563710@naver.com>
2022-11-21 13:39:51 +0900

    remove(rsp): remove unused components

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import MobileToastProvider from "@utils/common/mobile-toast/MobileToastProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <MobileToastProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </MobileToastProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );

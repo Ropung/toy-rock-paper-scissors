de1b7c0da53295634937ab73fa99099ea31e448c
merge-simpson <letsdev@kakao.com>
2022-11-23 01:45:52 +0900

    feat(bc): add routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import HomePage from "./components/home/HomePage";
 import GNB from "./components/common/nav/GNB";
 import RSPGameHome from "@components/rsp/start/RSPGameHome";
 import TicTacToePage from "@components/tic-tac-toe/TicTacToePage";
+import BullsAndCowsPage from "@components/bulls-and-cows/BullsAndCowsPage";
 
 function App() {
   const location = useLocation();
-  const { HOME, RSP, TTT } = Path;
+  const { HOME, RSP, TTT, BC } = Path;
 
   const [hasNav, setHasNav] = useState<boolean>(false);
 
@@ -30,6 +31,7 @@ function App() {
         <Route path={HOME} element={<HomePage />} />
         <Route path={RSP} element={<RSPGameHome />} />
         <Route path={TTT} element={<TicTacToePage />} />
+        <Route path={BC} element={<BullsAndCowsPage />} />
         {/* TODO redirect to HOME on wrong path */}
       </Routes>
     </div>
--- src/utils/routes/Path.ts
+++ src/utils/routes/Path.ts
@@ -2,6 +2,7 @@ const Path = {
   HOME: "/",
   RSP: "/rsp",
   TTT: "/tic-tac-toe",
+  BC: "/bulls-and-cows",
 };
 
 Object.freeze(Path);
